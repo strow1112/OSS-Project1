e6dab34c20dfcbc4f8580f1b7a6cc1805d5eb2de
박현수 <qkrgustn1112@naver.com>
2019-04-10 16:09:41 +0900

    마스터브랜치에 코드를 추가하였음

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-  printf("First OSS code");
-  printf("Third OSS code");
+  printf("First OSS code\n");
+  printf("Second OSS code\n");
+  printf("마스터브랜치에 코드를 추가하였음");
 
   return 0;
 }
